8d3d56d2c719ba991c52bde0385b67d2f9f10275
zhaokuo <zhaokuo_game@163.com>
2019-02-27 13:21:37 +0800

    ad bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+
+set -e
+set -x
+
+[ ! -d "$HOME/.local" ] && mkdir -p "$HOME/.local" 2> /dev/null
+cd $HOME/.local
+git clone https://github.com/kecookier/workspace.git
+
+# install .bashrc
+
+#cd ~/.vim/vim/etc
+#sh update.sh
+#
+#cd ~
+#
+#[ ! -d "$HOME/.config" ] && mkdir -p "$HOME/.config" 2> /dev/null
+#
+#
+#cp $SCRIPTPATH/*.sh "$HOME/.local/etc/" 
+#cp $SCRIPTPATH/*.conf "$HOME/.local/etc/" 
+#cp $SCRIPTPATH/*.fish "$HOME/.config/fish/"
+#cp $SCRIPTPATH/*.zsh "$HOME/.local/etc/"
+#cp $SCRIPTPATH/*.lua "$HOME/.local/etc/"
+#cp $SCRIPTPATH/inputrc "$HOME/.local/etc"
+#
+#cp $SCRIPTPATH/../tools/bin/* "$HOME/.local/bin"
+#cp $SCRIPTPATH/../lib/* "$HOME/.local/lib/python"
+#
+#echo 'source ~/.local/etc/init.sh' >> ~/.bashrc
