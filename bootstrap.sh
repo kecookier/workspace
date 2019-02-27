343459ff1ea85a15e7c517f73e81dfc96a33dea7
zhaokuo <zhaokuo_game@163.com>
2019-02-27 17:31:39 +0800

    modify bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,28 +4,15 @@
 set -e
 set -x
 
+echo "init git repo..."
 [ ! -d "$HOME/.local" ] && mkdir -p "$HOME/.local" 2>/dev/null
 cd $HOME/.local
 git clone https://github.com/kecookier/workspace.git
 
-# install .bashrc
+echo "install .bashrc.."
+echo 'source ~/.local/workspace/init.sh' >> ~/.bashrc
+echo "install .tmux.conf.."
+echo 'source ~/.local/workspace/tmux.config' >> ~/.tmux.conf
+echo "install .vimrc"
+echo 'source ~/.local/workspace/vimrc' >> ~/.vimrc
 
-#cd ~/.vim/vim/etc
-#sh update.sh
-#
-#cd ~
-#
-#[ ! -d "$HOME/.config" ] && mkdir -p "$HOME/.config" 2> /dev/null
-#
-#
-#cp $SCRIPTPATH/*.sh "$HOME/.local/etc/" 
-#cp $SCRIPTPATH/*.conf "$HOME/.local/etc/" 
-#cp $SCRIPTPATH/*.fish "$HOME/.config/fish/"
-#cp $SCRIPTPATH/*.zsh "$HOME/.local/etc/"
-#cp $SCRIPTPATH/*.lua "$HOME/.local/etc/"
-#cp $SCRIPTPATH/inputrc "$HOME/.local/etc"
-#
-#cp $SCRIPTPATH/../tools/bin/* "$HOME/.local/bin"
-#cp $SCRIPTPATH/../lib/* "$HOME/.local/lib/python"
-#
-#echo 'source ~/.local/etc/init.sh' >> ~/.bashrc
--- init.sh
+++ init.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+alias svnd="svn st | grep '^[MA]' --color"
+alias mantmux="man M /home/xx/local/share/man/man1/tmux.1"
+alias grep="grep --color"
+export PS1="\[\e[32m\][#\##\[\e[31m\]\u\[\e[32m\]@\[\e[31m\]\h \[\e[35m\]\w\[\e[32m\]]$\[\e[m\]"
 
-# user define
+#make vim not overwrite the content in window after vim exits
+export TERM=xterm
