49d7523b41ffeed7cba471ba965d07214480c4e7
zhaokuo <zhaokuo_game@163.com>
2019-03-09 14:54:10 +0800

    modify bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,21 @@
 #! /bin/sh
 #https://raw.githubusercontent.com/kecookier/workspace/master/bootstrap.sh
 
-set -e
-set -x
+#set -e
+#set -x
 
 echo "init git repo..."
 [ ! -d "$HOME/.local" ] && mkdir -p "$HOME/.local" 2>/dev/null
 cd $HOME/.local
-git clone https://github.com/kecookier/workspace.git
+if [ -d "$HOME/.local/workspace" ]
+then
+    cd $HOME/.local/workspace
+    git pull https://github.com/kecookier/workspace.git
+    echo "Already exist workspace, just update to oldest!"
+    exit
+else
+    git clone https://github.com/kecookier/workspace.git
+fi
 
 echo "install .bashrc.."
 echo 'source ~/.local/workspace/init.sh' >> ~/.bashrc
